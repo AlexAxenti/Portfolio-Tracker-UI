f176ac04251380c31057fa0e3a8368aeee904dd0
AlexAxenti <alex.axenti@gmail.com>
2026-06-06 09:39:41 -0400

    init route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'holdings',
+    pathMatch: 'full'
+  },
+  {
+    path: 'holdings',
+    loadComponent: () =>
+      import('./features/holdings/pages/holdings-page/holdings-page')
+        .then(m => m.HoldingsPage)
+  }
+];
