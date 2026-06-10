eb5c02aac81ceafe35c6dcc7495a2e0920ba6459
AlexAxenti <alex.axenti@gmail.com>
2026-06-10 09:01:44 -0400

    proper env var seperation

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:5011',
+  apiUrl: 'https://portfolio-api.168.119.115.41.nip.io',
   supabaseUrl: 'https://fofxhsskfehnylunwqyh.supabase.co',
   supabasePublishableKey: 'sb_publishable_7sWtMmW5TUFuUCaP3xZ8Wg_CVj2F_XY',
 };
