044746a35b77a25a7526bd200e613bf4a80dc0ad
AlexAxenti <alex.axenti@gmail.com>
2026-06-13 12:35:47 -0400

    Responding to rabbit mq additions

--- src/app/core/prices.service.ts
+++ src/app/core/prices.service.ts
@@ -2,21 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, catchError, throwError, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Holding } from '../features/holdings/models/holding.model';
-import { HoldingsService } from '../features/holdings/services/holdings.service';
 import { toReadableHttpError } from './http-error';
+
+export interface PriceRefreshQueuedResponse {
+  message: string;
+  queuedTickers: string[];
+  queuedAtUtc: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class PricesService {
   private readonly http = inject(HttpClient);
-  private readonly holdingsService = inject(HoldingsService);
   private readonly pricesUrl = `${environment.apiUrl}/api/prices`;
 
-  refreshPrices(): Observable<Holding[]> {
-    return this.http.post<Holding[]>(`${this.pricesUrl}/refresh-prices`, {}).pipe(
-      tap((holdings) => this.holdingsService.replaceHoldings(holdings)),
+  refreshPrices(): Observable<PriceRefreshQueuedResponse> {
+    return this.http.post<PriceRefreshQueuedResponse>(`${this.pricesUrl}/refresh-prices`, {}).pipe(
+      tap((response) => console.info('Price refresh queued.', response)),
       catchError((error) =>
         throwError(() => toReadableHttpError(error, 'Holding prices could not be refreshed.'))
       )
